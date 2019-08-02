49db004e39eeb3dedc9ff819c648228c7279f6c7
Andrei Vladescu <52881237+SorinSalam@users.noreply.github.com>
2019-08-02 21:34:52 +0300

    Add files via upload

--- proiect.ino
+++ proiect.ino
@@ -1,26 +1,18 @@
-int t1 = 12;  
-int t2 = 2;
-int t3 = 5;
-int t4 = 7; 
-  
-int e1 = 13;   
-int e2 = 4;
-int e3 = 6;
-int e4 = 8;
+// Redarea numelor pinilor : t - Trigger, e - Echo, M - Motor, B - Buzzer
+int t1 = 12 ,t2 = 2, t3 = 5, t4 = 7; 
 
-int m1 = 3;
-int m2 = 9;
-int m3 = 11;
-int b = 10;
+int e1 = 13, e2 = 4, e3 = 6, e4 = 8;
 
-// TRIGGER apoi ECHO
+int m1 = 3, m2 = 9, m3 = 11, b = 10;
+
+// Initializarea variabilelor ce vor stoca lungimi
 long d1,d2,d3, cm1,cm2,cm3,h,cm4;
  
 void setup() {
-  //Serial Port begin
+  //Initializare comunicare serial cu 9600 bits/s
   Serial.begin (9600);
-  //Define inputs and outputs
-  pinMode(t1, OUTPUT);
+  //Definirea pinilor ca stare
+   pinMode(t1, OUTPUT);
   pinMode(e1, INPUT);
    pinMode(t2, OUTPUT);
   pinMode(e2, INPUT);
@@ -28,45 +20,47 @@ void setup() {
   pinMode(e3, INPUT);
    pinMode(t4, OUTPUT);
   pinMode(e4, INPUT);
+   pinMode(m1,OUTPUT); 
+   pinMode(m2,OUTPUT); 
+   pinMode(m3,OUTPUT); 
+   pinMode(b, OUTPUT);
 
-  pinMode(m1,OUTPUT); //make pin 3 an output
-  pinMode(m2,OUTPUT); 
-  pinMode(m3,OUTPUT); 
-  pinMode(b, OUTPUT); // Set buzzer - pin 9 as an output
-  
-  TCCR2B = (TCCR2B & 0xF8) | 2; //set PWM frequency to 3906 Hz for pin 3 (and 11)
-  TCCR1B = (TCCR1B & 0xF8) | 2;  // PWM freq of 9 and 10
+  // Setarea frecventei la 3906 Hz 
+  TCCR2B = (TCCR2B & 0xF8) | 2;  // Frecventa PWM pentru pinii 3 si 11 
+  TCCR1B = (TCCR1B & 0xF8) | 2;  // Frecventa PWM pentru pinii 9 si 10
 }
  
 void loop() {
+  // Procedeul de initializare al pinului Trigger
   digitalWrite(t1, LOW);
   delayMicroseconds(2);
   digitalWrite(t1, HIGH);
   delayMicroseconds(10);
   digitalWrite(t1, LOW);
- 
-  pinMode(e1, INPUT);
+  // Preluarea semnalului de la pinul Echo
+  // Incarcarea datelor in variabila d1 
   d1 = pulseIn(e1, HIGH);
- 
+  // Procesarea informatiei in functie de viteza sunetului prin aer
   cm1 = (d1/2) / 29.1;  
-    
-  if (cm1<50) 
+
+  // 
+  if (cm1<60) 
   {
-     analogWrite(m1,250); //do 50% PWM on pin 3 at the frequency set in TCCR2B 
+     analogWrite(m1,255); //Se produce un semnal de 100% PWM pentru motorul 1
      delay (5);
   }
-  else if (cm1<100)
+  else if (cm1<110)
   {
-    analogWrite(m1,100);
+    analogWrite(m1,200); //Se produce un semnal de 78% PWM pentru motorul 1
     delay (5);
   }
-  else if (cm1>100)
+  else if (cm1<130)
   {
-    analogWrite(m1,1);
+    analogWrite(m1,80); // Se produce un semnal de 30% PWM pentru motorul 1
   }
-
+  else analogWrite(m1,0); // Daca niciuna dintre ramuri nu a fost atinsa motorul ramane stins
 //////////////////////////////////////////////
-  delay(1); 
+  delay(1);  // Intarziere de o milisecunda
 //////////////////////////////////////////////
 digitalWrite(t2, LOW);
   delayMicroseconds(2);
@@ -79,20 +73,21 @@ digitalWrite(t2, LOW);
  
   cm2 = (d2/2) / 29.1;  
     
-  if (cm2<50) 
+  if (cm2<60) 
   {
-     analogWrite(m2,250); //do 50% PWM on pin 3 at the frequency set in TCCR2B 
+     analogWrite(m2,255); //do 50% PWM on pin 3 at the frequency set in TCCR2B 
      delay (5);
   }
-  else if (cm2<100)
+  else if (cm2<110)
   {
-    analogWrite(m2,100);
+    analogWrite(m2,200);
     delay (5);
   }
-   else if (cm2>100)
+   else if (cm2<130)
   {
-    analogWrite(m2,1);
+    analogWrite(m2,80);
   }
+  else analogWrite(m2,0);
  
 /////////////////////////////////////////////////
   delay(1); 
@@ -108,20 +103,21 @@ digitalWrite(t3, LOW);
  
   cm3 = (d3/2) / 29.1;   
 
-   if (cm3<50) 
+   if (cm3<60) 
   {
-     analogWrite(m3,250); //do 50% PWM on pin 3 at the frequency set in TCCR2B 
+     analogWrite(m3,255);
      delay (5);
   }
-  else if (cm3<100)
+  else if (cm3<110)
   {
-    analogWrite(m3,100);
+    analogWrite(m3,200);
     delay (5);
   }
-  else if (cm3>100)
+  else if (cm3<130)
   {
-    analogWrite(m3,1);
+    analogWrite(m3,80);
   }
+   else analogWrite(m3,0);
 /////////////////////////////////////////////////
   delay(1); 
 /////////////////////////////////////////////////
@@ -136,22 +132,24 @@ digitalWrite(t4, LOW);
  
   cm4 = (h/2) / 29.1;  
 
-   if (cm4>350)
+   if (cm4>100)
    {
-    delay(1);
+    tone(b, 2000); // Ton de 2kHz
+    delay(20);        
+    noTone(b);     // Incetare sunet
    }
-  else if (cm4>60) 
+  else if (cm4>70) 
   {
      
-    tone(b, 1000); // Send 1KHz sound signal...
+    tone(b, 1000); // Ton de 1kHz
     delay(20);        
-    noTone(b);     // Stop sound...
+    noTone(b);     // Incetare sunet
   }
-  else if (cm4>40)
+  else if (cm4>50)
   {
-    analogWrite(b,500);
+    tone(b,500); // Ton de 500Hz
     delay (20);
-    noTone(b);     // Stop sound...
+    noTone(b);     // Incetare sunet
   }
  
 ///////////////////////////////////////////////
